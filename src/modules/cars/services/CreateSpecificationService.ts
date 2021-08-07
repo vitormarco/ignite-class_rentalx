67e032f205b853af5d0823357e95cfc751e963b6
vitormarco <vitoradrianomarco@gmail.com>
2021-08-07 15:48:58 -0300

    Add Specification module

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,22 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute(): void {
-    console.log("TODO");
+  constructor(private specificationsRepository: ISpecificationRepository) {}
+
+  execute({ description, name }: IRequest): void {
+    const specificationsAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationsAlreadyExists) {
+      throw new Error("Specification already exist");
+    }
+
+    this.specificationsRepository.create({ name, description });
   }
 }
 
